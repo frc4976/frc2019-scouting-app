686986b3e8b51d7b2d6f94f542c36007f116140c
Adrian Lloyd <1lloydadr@hdsb.ca>
2019-02-02 15:09:48 -0500

    Added titles for the csv file.

---
+++ lib/aLotOfVariables.dart
@@ -0,0 +1,12 @@
+String teamNumber1 = '';
+String team1Pos;
+
+String teamNumber2 = '';
+
+String teamNumber3 = '';
+
+String teamNumber4 = '';
+
+String teamNumber5 = '';
+
+String teamNumber6 = '';
